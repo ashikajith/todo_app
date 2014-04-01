d51847f4be3d4981a95774f2e9e6216db4f9d06b
ashikajith <ashikajith@gmail.com>
2014-04-01 19:40:22 +0530

    Adding routes for sorting

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 TodoApp::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
-  resources :todos
+  resources :todos do
+    collection do
+      post 'sort'
+    end
+  end  
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
