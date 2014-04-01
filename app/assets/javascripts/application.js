9f282ac33191e6bd8581048a4eef8a99908b01cb
ashikajith <ashikajith@gmail.com>
2014-04-01 19:39:15 +0530

    Adding code for sortable jquery and the styles

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,34 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.ui.all
 //= require twitter/bootstrap
 //= require_tree .
+
+// $(document).ready(function(){
+// 	$('#list').hover(function(){
+// 		$('#link_hidden').show();
+// 	}, function(){
+// 		$('#link_hidden').hide();
+// 	});
+// });
+
+$(document).ready(function(){
+	  $('#item_lists').sortable({
+	    dropOnEmpty: false,
+	    cursor: 'crosshair',
+	    opacity: 0.4,
+	    scroll: true,
+	    update: function(){
+	    $.ajax({
+	      type: 'post',
+	      data: $('#item_lists').sortable('serialize'),
+	      url: '/todos/sort',
+	      dataType: 'script',
+	      complete: function(request){
+	        $('#item_lists').effect('highlight');
+	      },
+	    })
+	  }
+  });
+});
